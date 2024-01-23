6efa6ff1e4e10871233b56adb43c6643be9a796e
rodersnow <rodersnow@gmail.com>
2024-01-23 12:49:20 +0100

    update getUserInput

--- Project2/src/Main.cpp
+++ Project2/src/Main.cpp
@@ -18,6 +18,18 @@ std::vector,
 std::istringstream,
 std::ios;
 
+
+template<typename T>
+void getUserInput(const string& prompt, T& input) {
+	cout << prompt;
+	
+	if(!(cin >> input)){
+		cin.clear();
+		cin.ignore(100, '\n');
+	}
+}
+
+
 class BookService {
 private:
 		BookRepository bookRepository;
@@ -42,26 +54,30 @@ bool isRunning = true;
 int main() {
 	while (isRunning) {
 		displayMenu();
-		getUserInput(choice);
+		getUserInput("Enter your choice (1-4): ", choice);
 
 		switch (choice) {
 		case 1:
+		{
 			cout << endl;
 			displayBooks(bookService.getAll());
 			break;
-
+		}
 		case 2:
-			cout << "Create new book\n";
+		{
 			break;
+		}
 
 		case 3:
 			cout << "Delete book\n";
 			break;
 
 		case 4:
+		{
 			cout << "Exiting program...\n";
 			isRunning = false;
 			break;
+		}
 
 		default:
 			cout << "Invalid input. Please try again." << '\n';
