fb92f01df9f69b5d48564fd3e1af1fc08558859b
rodersnow <rodersnow@gmail.com>
2024-01-25 10:16:14 +0100

    create UserRepository class

---
+++ Book-collection/include/UserRepository.h
@@ -0,0 +1,27 @@
+#include "User.h"
+#include <vector>
+#include <string>
+#include <cppconn/driver.h>
+#include <cppconn/exception.h>
+#include <cppconn/prepared_statement.h>
+#include <mysql_connection.h>
+#include <mysql_driver.h>
+
+using
+std::vector,
+std::string,
+std::cout,
+std::endl;
+
+class BookRepository {
+private:
+    sql::Driver* driver;
+    sql::Connection* con;
+    sql::PreparedStatement* prep_stmt;
+    sql::ResultSet* res;
+
+
+public:
+    BookRepository();
+    ~BookRepository();
+};
